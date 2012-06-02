f02e25e73840e9cca346710233b3cea6e4f0fce9
Matthew Mitchell <matthewmitchell@godofgod.co.uk>
2012-06-02 23:39:30 +0100

    Fixed CBVarInt problems

--- cbitcoin/src/structures/CBVarInt/CBVarInt.c
+++ cbitcoin/src/structures/CBVarInt/CBVarInt.c
@@ -34,15 +34,15 @@ CBVarInt CBVarIntDecode(CBByteArray * bytes,u_int32_t offset){
 	} else if (first == 253) {
 		// 16 bits.
 		result.val = CBGetByteArrayVT(bytes)->readUInt16(bytes,offset+1);
-		result.size = 2;
+		result.size = 3;
 	} else if (first == 254) {
 		// 32 bits.
 		result.val = CBGetByteArrayVT(bytes)->readUInt32(bytes,offset+1);
-		result.size = 4;
+		result.size = 5;
 	} else {
 		// 64 bits.
 		result.val = CBGetByteArrayVT(bytes)->readUInt64(bytes,offset+1);
-		result.size = 8;
+		result.size = 9;
 	}
 	return result;
 }
@@ -51,15 +51,15 @@ void CBVarIntEncode(CBByteArray * bytes,u_int32_t offset,CBVarInt varInt){
 		case 1:
 			CBGetByteArrayVT(bytes)->setByte(bytes,offset,(u_int8_t)varInt.val);
 			break;
-		case 2:
+		case 3:
 			CBGetByteArrayVT(bytes)->setByte(bytes,offset,253);
 			CBGetByteArrayVT(bytes)->setUInt16(bytes,offset+1,(u_int16_t)varInt.val);
 			break;
-		case 4:
+		case 5:
 			CBGetByteArrayVT(bytes)->setByte(bytes,offset,254);
 			CBGetByteArrayVT(bytes)->setUInt32(bytes,offset+1,(u_int32_t)varInt.val);
 			break;
-		case 8:
+		case 9:
 			CBGetByteArrayVT(bytes)->setByte(bytes,offset,255);
 			CBGetByteArrayVT(bytes)->setUInt64(bytes,offset+1,varInt.val);
 			break;
@@ -76,6 +76,8 @@ u_int8_t CBVarIntSizeOf(u_int64_t value){
 		return 1;
 	else if (value < 65536)
 		return 3;  // 1 marker + 2 data bytes
-	return 5;  // 1 marker + 4 data bytes
+	else if (value < 4294967296)
+		return 5;  // 1 marker + 4 data bytes
+	return 9; // 1 marker + 4 data bytes
 }
 
