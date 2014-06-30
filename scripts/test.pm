8b77b22322431e03fbcbd49127255f20f06280f4
Joel DeJesus <dejesus.joel@e-flamingo.jp>
2014-06-30 22:31:15 +0900

    fixing multisig function in CBScripts

--- scripts/test.pm
+++ scripts/test.pm
@@ -5,27 +5,28 @@
  use CBitcoin::Script;
  use CBitcoin::CBHD;
 print "hello\n";
-
+my ($m,$n) = (1,2);
 
 my $script = CBitcoin::Script::address_to_script("1Fjf3xLuuQpuyw6EyxkXuLHzJUw7yUwPTi");
 print "Script:$script\n";
 
 my $parentkey = new CBitcoin::CBHD;
-$parentkey->generate();
+$parentkey->serializedkeypair('xprv9s21ZrQH143K4Rz5APz2LhW4ms2mVTVa5YVMZzGAYgNRoXkxri6ELZVbzqc8VFtHseksaBnahJbbgkxue3nXsMjuF5pg1cknX4ueyQwUATY');
 $script = CBitcoin::Script::pubkeyToScript($parentkey->publickey());
-print "Script from public key:$script\n";
+my $scripttype = CBitcoin::Script::whatTypeOfScript($script);
+print "Script from public key:$script\nType:$scripttype\n";
 #my $childkey = $parentkey->deriveChild(1,23);
 
 my @arraypubkeys;
-foreach my $i (1..3){
+foreach my $i (1..$n){
 	my $childkey = $parentkey->deriveChild(1,$i);
 	print "Address $i:".$childkey->address()."\n";
 	push(@arraypubkeys,$childkey->publickey());
 }
 print "Starting multisig operation\n";
-$script = CBitcoin::Script::multisigToScript(\@arraypubkeys,2,3);
-print "Multisig Script:$script\n";
-my $scripttype = CBitcoin::Script::whatTypeOfScript($script);
+$script = CBitcoin::Script::multisigToScript(\@arraypubkeys,$m,$n);
+print "Multisig Script:\n($script)\n";
+$scripttype = CBitcoin::Script::whatTypeOfScript($script);
 
 print "Script Type:$scripttype\n";
 
