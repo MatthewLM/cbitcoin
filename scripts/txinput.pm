4430e53ab3863d540ad7e1ab60844ec38b8eae9e
Joel DeJesus <dejesus.joel@e-flamingo.jp>
2014-10-04 20:39:05 +0900

    TxInput working

--- scripts/txinput.pm
+++ scripts/txinput.pm
@@ -4,37 +4,54 @@
  use Digest::SHA qw(sha256);
  use CBitcoin::Script;
  use CBitcoin::TransactionInput;
+ use CBitcoin::TransactionOutput;
 print "hello\n";
 
-my $address = "1Fjf3xLuuQpuyw6EyxkXuLHzJUw7yUwPTi";
-print "Address:$address\n";
-my $script = CBitcoin::Script::address_to_script($address);
-print "Script:$script\n";
-$address = CBitcoin::Script::script_to_address($script);
-print "Address:$address\n";
 for (<DATA>) {
 	next if /^#/;
 	chomp;
 	eval {
-		my ($address,$blah) = split(',',$_);
-		print "Address:[$address]\n";
+		use bigint;
+		my ($address,$value,$prevOutHash,$prevOutIndex) = split(',',$_);
+
 		my $script = CBitcoin::Script::address_to_script($address);
-		my $newaddress = CBitcoin::Script::script_to_address($script);
+		#my $newaddress = CBitcoin::Script::script_to_address($script);
+		#my $out = CBitcoin::TransactionOutput->new({'script' => $script, 'value' => $value});
+		my $in = CBitcoin::TransactionInput->new({
+			'prevOutHash' => $prevOutHash
+			,'prevOutIndex' => $prevOutIndex
+			,'script' => $script
+		});
+					
+
 		my $bool = 0;
-		$bool = 1 if $address eq $newaddress;
+		if(
+			$prevOutHash eq $in->prevOutHash()
+			&& $prevOutIndex eq $in->prevOutIndex()
+			&& $script eq $in->script()
+		){
+			$bool = 1;
+		}
+
+
+#		$bool = 1 if $script eq $out->script() && $value eq $out->value() && $address eq CBitcoin::Script::script_to_address($out->script());
+		
 		print "Test:($script|$bool)\n";
 	};
 	if($@){ warn "Error:$@";}
 }
+
+
+
 __DATA__
 # The following keys were created using javascript code on bitaddress.org
-16PRgUZvCneM7AYJ94TaoGE4rMQnRdqqt4,5JkYWudPXkXHkmayfUz52WsweFpY7saT4V9vh4ZNtWgFLLwNgej
-1KDxAxej4NZMQtao9xZGiadbsqxcKJt9Ng,5J4G4vaBCiAF881AMEEyc8uC2EHgLfCFP4BL97EvD5vnH99yC6p
-1PnA88ck7hGSsSqpPXhaVbWL3suWXEqfsF,5KKGiz5ViCpSXzWCm9ff48g5AdK54FR3w1ByrhQDb1U6kgjmgr2
-12jXM28Awqgm2NPgiD6EVjZmih66U5mUAt,5JfTp8uzBFzcJWhjrb7wJfevehwjT6c3WCBcYivpoRcjaSAgHtZ
-1BV1GXQmBKF6v6CqUjH6y95KNXqtCoLNWH,5JGcHNTEqrYGVuE2sRw2Ys26xY51ypm4c1dnfZGyw8naAUjkdGM
-1AsCoc5UVkHcfFRXUA2YtDJbse7k6Ws4Pz,5J3fMYfP7knUYk67eETrQcby9vhUHjdH9mz5Veag4TvuhX9JMnF
-1MNJYvW6AmRhsrr9ELo5A4mRw2P4yafVZQ,5JEwVA4ek7Kcm23oPKaJEx6dj9G9UcM6DCybSb3hhaG8875L3hb
-1HCMAqVqJEYNq444ecHeYJ235soup8wUKb,5KVZt4CfaBhPHaJ9Bw9b9zoM7TfnPBTwz3SyYBcn2fEdwHfrzJF
-1tkf9nKFNqqD5FNyxU6C4CFBtk8dQFFLw,5JQvrDNPZeLN78jYPpscv3x15pgqDDdkMBwjzva3PMCBagop2yA
-1NwuhznLyRaQ7e3hWpFveMVimsH5FfdhLa,5K2dPvJfj2MUvfi5zGxPvUupqPVR2o6PqEtsHzfVPregUoYKUBE
+16PRgUZvCneM7AYJ94TaoGE4rMQnRdqqt4,5453543543,3f12ca91d24d3ff8eb89058c3ab05433419ea069ed31b754eaf4afa7486f4f8f,0
+1KDxAxej4NZMQtao9xZGiadbsqxcKJt9Ng,1000933,3f12ca91d24d3ff8eb89058c3ab05433419ea069ed31b754eaf4afa7486f4f8f,0
+1PnA88ck7hGSsSqpPXhaVbWL3suWXEqfsF,100033223,3f12ca91d24d3ff8eb89058c3ab05433419ea069ed31b754eaf4afa7486f4f8f,2
+12jXM28Awqgm2NPgiD6EVjZmih66U5mUAt,99599595,3f12ca91d24d3ff8eb89058c3ab05433419ea069ed31b754eaf4afa7486f4f8f,5
+1BV1GXQmBKF6v6CqUjH6y95KNXqtCoLNWH,345445,3f12ca91d24d3ff8eb89058c3ab05433419ea069ed31b754eaf4afa7486f4f8f,0
+1AsCoc5UVkHcfFRXUA2YtDJbse7k6Ws4Pz,20000003,3f12ca91d24d3ff8eb89058c3ab05433419ea069ed31b754eaf4afa7486f4f8f,0
+1MNJYvW6AmRhsrr9ELo5A4mRw2P4yafVZQ,20000111,3f12ca91d24d3ff8eb89058c3ab05433419ea069ed31b754eaf4afa7486f4f8f,1
+1HCMAqVqJEYNq444ecHeYJ235soup8wUKb,30000003,3f12ca91d24d3ff8eb89058c3ab05433419ea069ed31b754eaf4afa7486f4f8f,0
+1tkf9nKFNqqD5FNyxU6C4CFBtk8dQFFLw,400000003,3f12ca91d24d3ff8eb89058c3ab05433419ea069ed31b754eaf4afa7486f4f8f,0
+1NwuhznLyRaQ7e3hWpFveMVimsH5FfdhLa,66666555,3f12ca91d24d3ff8eb89058c3ab05433419ea069ed31b754eaf4afa7486f4f8f,10
