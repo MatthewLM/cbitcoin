7adb0ef5feacc8b4d86b500b4bc1bb316d1b712b
Joel DeJesus <dejesus.joel@e-flamingo.jp>
2014-10-16 09:42:29 +0900

    trying to do automake

--- scripts/caller.pm
+++
@@ -1,7 +0,0 @@
- use CBitcoin::CBHD;
-
-my $masterkey = new CBitcoin::CBHD;
-$masterkey->generate();
-my $wif = $masterkey->WIF();
-
-print "Wif:$wif\n";
--- scripts/test2.pm
+++
@@ -1,5 +0,0 @@
- use CBitcoin::Script;
-
-my $address = '1ErayvrvUhxw1Nr6gkWmqVHUNZdX5QxxjY';
-
-print "Script:".CBitcoin::Script::addressToScript($address)."\n";
