66aefde129a6a57889d38038ba81ab23c25d2bb4
Joel DeJesus <dejesus.joel@e-flamingo.jp>
2015-01-29 08:58:56 +0900

    client server stuff

---
+++ src/fullnode.c
@@ -0,0 +1,344 @@
+//
+//  testCBNetworkCommunicator.c
+//  cbitcoin
+//
+//  Created by Matthew Mitchell on 10/08/2012.
+//  Copyright (c) 2012 Matthew Mitchell
+//
+//  This file is part of cbitcoin. It is subject to the license terms
+//  in the LICENSE file found in the top-level directory of this
+//  distribution and at http://www.cbitcoin.com/license.html. No part of
+//  cbitcoin, including this file, may be copied, modified, propagated,
+//  or distributed except according to the terms contained in the
+//  LICENSE file.
+
+#include <stdio.h>
+#include "CBNetworkCommunicator.h"
+#include "CBLibEventSockets.h"
+#include <event2/thread.h>
+#include <time.h>
+#include "stdarg.h"
+
+typedef enum{
+	GOTVERSION = 1,
+	GOTACK = 2,
+	GOTPING = 4,
+	GOTPONG = 8,
+	GOTGETADDR = 16,
+	COMPLETE = 32,
+}TesterProgress;
+
+typedef struct{
+	TesterProgress prog[7];
+	CBPeer * peerToProg[7];
+	uint8_t progNum;
+	int complete;
+	int addrComplete;
+	CBNetworkCommunicator * comms[3];
+	pthread_mutex_t testingMutex;
+}Tester;
+
+pthread_cond_t nodeEndCond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t nodeEndMutex = PTHREAD_MUTEX_INITIALIZER;
+int ended = 0;
+
+uint64_t CBGetMilliseconds(void){
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
+bool onTimeOut(CBNetworkCommunicator * comm, void * peer);
+bool onTimeOut(CBNetworkCommunicator * comm, void * peer){
+	CBLogError("TIMEOUT FAIL");
+	CBNetworkCommunicatorStop((CBNetworkCommunicator *)comm);
+	return false;
+}
+
+void stop(void * comm);
+void stop(void * comm){
+	CBNetworkCommunicatorStop(comm);
+	pthread_mutex_lock(&nodeEndMutex);
+	if (++ended == 3)
+		pthread_cond_broadcast(&nodeEndCond);
+	pthread_mutex_unlock(&nodeEndMutex);
+}
+
+bool acceptType(CBNetworkCommunicator *, CBPeer *, CBMessageType);
+bool acceptType(CBNetworkCommunicator * comm, CBPeer * peer, CBMessageType type){
+	if (type == CB_MESSAGE_TYPE_VERACK && peer->handshakeStatus & CB_HANDSHAKE_GOT_ACK) {
+		CBLogError("ALREADY HAVE ACK FAIL\n");
+		exit(EXIT_FAILURE);
+	}
+	if (type == CB_MESSAGE_TYPE_VERSION && peer->handshakeStatus & CB_HANDSHAKE_GOT_VERSION) {
+		CBLogError("ALREADY HAVE VERSION FAIL\n");
+		exit(EXIT_FAILURE);
+	}
+	return true;
+}
+
+Tester tester;
+
+CBOnMessageReceivedAction onMessageReceived(CBNetworkCommunicator * comm, CBPeer * peer, CBMessage * theMessage);
+CBOnMessageReceivedAction onMessageReceived(CBNetworkCommunicator * comm, CBPeer * peer, CBMessage * theMessage){
+	puts("on Message Received.");
+	pthread_mutex_lock(&tester.testingMutex); // Only one processing of test at a time.
+	// Assign peer to tester progress.
+	uint8_t x = 0;
+	for (; x < tester.progNum; x++)
+		if (tester.peerToProg[x] == peer)
+			break;
+	if (x == tester.progNum) {
+		tester.peerToProg[x] = peer;
+		tester.progNum++;
+	}
+	TesterProgress * prog = tester.prog + x;
+	switch (theMessage->type) {
+		case CB_MESSAGE_TYPE_VERSION:
+			if (! ((peer->handshakeStatus & CB_HANDSHAKE_SENT_VERSION && *prog == GOTACK) || (*prog == 0))) {
+				CBLogError("VERSION FAIL");
+				exit(EXIT_FAILURE);
+			}
+			if (CBGetVersion(theMessage)->services) {
+				CBLogError("VERSION SERVICES FAIL");
+				exit(EXIT_FAILURE);
+			}
+			if (CBGetVersion(theMessage)->version != CB_PONG_VERSION) {
+				CBLogError("VERSION VERSION FAIL");
+				exit(EXIT_FAILURE);
+			}
+			if (memcmp(CBByteArrayGetData(CBGetVersion(theMessage)->userAgent), CB_USER_AGENT_SEGMENT, CBGetVersion(theMessage)->userAgent->length)) {
+				CBLogError("VERSION USER AGENT FAIL");
+				exit(EXIT_FAILURE);
+			}
+			if (memcmp(CBByteArrayGetData(CBGetVersion(theMessage)->addSource->sockAddr.ip), (uint8_t [16]){0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xFF, 0xFF, 127, 0, 0, 1}, 16)) {
+				CBLogError("VERSION SOURCE IP FAIL");
+				exit(EXIT_FAILURE);
+			}
+			if (memcmp(CBByteArrayGetData(CBGetVersion(theMessage)->addRecv->sockAddr.ip), (uint8_t [16]){0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xFF, 0xFF, 0, 0, 0, 0}, 16)) {
+				CBLogError("VERSION RECEIVE IP FAIL");
+				exit(EXIT_FAILURE);
+			}
+			*prog |= GOTVERSION;
+			break;
+		case CB_MESSAGE_TYPE_VERACK:
+			if (! (peer->handshakeStatus & CB_HANDSHAKE_SENT_VERSION)) {
+				CBLogError("VERACK FAIL = %u", peer->handshakeStatus);
+				exit(EXIT_FAILURE);
+			}
+			*prog |= GOTACK;
+			break;
+		case CB_MESSAGE_TYPE_PING:
+			if (! (*prog & GOTVERSION && *prog & GOTACK)) {
+				CBLogError("PING FAIL");
+				exit(EXIT_FAILURE);
+			}
+			CBGetPingPong(theMessage)->ID = CBGetPingPong(theMessage)->ID; // Test access to ID.
+			*prog |= GOTPING;
+			break;
+		case CB_MESSAGE_TYPE_PONG:
+			if (! (*prog & GOTVERSION && *prog & GOTACK)) {
+				CBLogError("PONG FAIL");
+				exit(EXIT_FAILURE);
+			}
+			CBGetPingPong(theMessage)->ID = CBGetPingPong(theMessage)->ID; // Test access to ID.
+			// At least one of the pongs worked.
+			*prog |= GOTPONG;
+			break;
+		case CB_MESSAGE_TYPE_GETADDR:
+			if (! (*prog & GOTVERSION && *prog & GOTACK)) {
+				CBLogError("GET ADDR FAIL");
+				exit(EXIT_FAILURE);
+			}
+			*prog |= GOTGETADDR;
+			break;
+		case CB_MESSAGE_TYPE_ADDR:
+			if (! (*prog & GOTVERSION && *prog & GOTACK)) {
+				CBLogError("ADDR FAIL");
+				exit(EXIT_FAILURE);
+			}
+			tester.addrComplete++;
+			break;
+		default:
+			CBLogError("MESSAGE FAIL");
+			exit(EXIT_FAILURE);
+			break;
+	}
+	if (*prog == (GOTVERSION | GOTACK | GOTPING | GOTPONG | GOTGETADDR)) {
+		*prog |= COMPLETE;
+		tester.complete++;
+	}
+	if (tester.addrComplete > 7) {
+		// addrComplete can be seven in the case that one of the listening nodes has connected to the other and then the connecting node asks the other node what addresses it has and it has the other node.
+		CBLogError("ADDR COMPLETE FAIL");
+		exit(EXIT_FAILURE);
+	}
+	if (tester.complete == 6) {
+		if (tester.addrComplete > 4) {
+			// Usually addrComplete will be 6 but sometimes addresses are not sent in response to getaddr when the address is being connected to. In reality this is not a problem, as it is seldom an address will be in a connecting state.
+			// Completed testing
+			CBLogVerbose("DONE");
+			CBLogVerbose("STOPPING COMM L1");
+			CBRunOnEventLoop(tester.comms[0]->eventLoop, stop, tester.comms[0], false);
+			CBLogVerbose("STOPPING COMM L2");
+			CBRunOnEventLoop(tester.comms[1]->eventLoop, stop, tester.comms[1], false);
+			CBLogVerbose("STOPPING COMM CN");
+			CBRunOnEventLoop(tester.comms[2]->eventLoop, stop, tester.comms[2], false);
+			return CB_MESSAGE_ACTION_CONTINUE;
+		}else{
+			CBLogError("ADDR COMPLETE DURING COMPLETE FAIL");
+			exit(EXIT_FAILURE);
+		}
+	}
+	pthread_mutex_unlock(&tester.testingMutex);
+	return CB_MESSAGE_ACTION_CONTINUE;
+}
+void onNetworkError(CBNetworkCommunicator * comm, CBErrorReason reason);
+void onNetworkError(CBNetworkCommunicator * comm, CBErrorReason reason){
+	if(reason == CB_ERROR_UNREACHABLE){
+		CBLogError("network error- unreachable");
+	}
+	else if(reason == CB_ERROR_NO_PEERS){
+		CBLogError("network error- no peers");
+	}
+	else if(reason == CB_ERROR_BAD_TIME){
+		CBLogError("network error- bad time");
+	}
+	else if(reason == CB_ERROR_VALIDATION){
+		CBLogError("network error- validation");
+	}
+	else if(reason == CB_ERROR_GENERAL){
+		CBLogError("network error- general");
+	}
+	CBLogError("DID LOSE LAST NODE");
+	exit(EXIT_FAILURE);
+}
+void onBadTime(void * foo);
+void onBadTime(void * foo){
+	CBLogError("BAD TIME FAIL");
+	exit(EXIT_FAILURE);
+}
+void onPeerWhatever(CBNetworkCommunicator * foo, CBPeer * bar);
+void onPeerWhatever(CBNetworkCommunicator * foo, CBPeer * bar){
+	return;
+}
+
+void onPeerFree(void * peer);
+void onPeerFree(void * peer){
+	return;
+}
+
+void CBNetworkCommunicatorTryConnectionsVoid(void * comm);
+void CBNetworkCommunicatorTryConnectionsVoid(void * comm){
+	CBNetworkCommunicatorTryConnections(comm, false);
+}
+
+void CBNetworkCommunicatorStartListeningVoid(void * comm);
+void CBNetworkCommunicatorStartListeningVoid(void * comm){
+	CBNetworkCommunicatorStartListening(comm);
+}
+
+int main(){
+	puts("You may need to move your mouse around if this test stalls.");
+	//puts("Not bothering to test network communicator.");
+	//return EXIT_SUCCESS;
+	memset(&tester, 0, sizeof(tester));
+	pthread_mutex_init(&tester.testingMutex, NULL);
+	CBByteArray * loopBack = CBNewByteArrayWithDataCopy((uint8_t [16]){0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xFF, 0xFF, 127, 0, 0, 1}, 16);
+	CBByteArray * localbtc = CBNewByteArrayWithDataCopy((uint8_t [16]){0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xFF, 0xFF, 10, 21, 0, 189}, 16);
+	CBNetworkAddress * addrConnect = CBNewNetworkAddress(0, (CBSocketAddress){localbtc, 8333}, 0, false); // Different port over loopback to seperate the CBNetworkCommunicators.
+	CBByteArray * userAgent3 = CBNewByteArrayFromString(CB_USER_AGENT_SEGMENT, false);
+	CBNetworkCommunicatorCallbacks callbacks = {
+		onPeerWhatever,
+		acceptType,
+		onMessageReceived,
+		onNetworkError
+	};
+
+	CBNetworkAddressManager * addrManConnect = CBNewNetworkAddressManager(onBadTime);
+	addrManConnect->maxAddressesInBucket = 4;
+	CBNetworkCommunicator * commConnect = CBNewNetworkCommunicator(0, callbacks);
+	CBNetworkCommunicatorSetReachability(commConnect, CB_IP_IP4 | CB_IP_LOCAL, true);
+	addrManConnect->callbackHandler = commConnect;
+	commConnect->networkID = CB_PRODUCTION_NETWORK_BYTES;
+	commConnect->flags = CB_NETWORK_COMMUNICATOR_AUTO_HANDSHAKE | CB_NETWORK_COMMUNICATOR_AUTO_PING | CB_NETWORK_COMMUNICATOR_AUTO_DISCOVERY;
+	commConnect->version = CB_PONG_VERSION;
+	commConnect->maxConnections = 2;
+	commConnect->maxIncommingConnections = 0;
+	commConnect->heartBeat = 2000;
+	commConnect->timeOut = 3000;
+	commConnect->recvTimeOut = 1000;
+	CBNetworkCommunicatorSetAlternativeMessages(commConnect, NULL, NULL);
+	CBNetworkCommunicatorSetNetworkAddressManager(commConnect, addrManConnect);
+	CBNetworkCommunicatorSetUserAgent(commConnect, userAgent3);
+	CBNetworkCommunicatorSetOurIPv4(commConnect, addrConnect);
+	// Release objects
+	CBReleaseObject(userAgent3);
+	// Give tester communicators
+	//tester.comms[0] = commListen;
+	//tester.comms[1] = commListen2;
+	//tester.comms[2] = commConnect;
+	/*
+	// Start listening on first listener. Can start listening on this thread since the event loop will not be doing anything.
+	CBRunOnEventLoop(commListen->eventLoop, CBNetworkCommunicatorStartListeningVoid, commListen, true);
+	if (! commListen->ipData[CB_IP4_NETWORK].isListening) {
+		CBLogError("FIRST LISTEN FAIL");
+		exit(EXIT_FAILURE);
+	}
+	// Start listening on second listener.
+	CBRunOnEventLoop(commListen2->eventLoop, CBNetworkCommunicatorStartListeningVoid, commListen2, true);
+	if (! commListen2->ipData[CB_IP4_NETWORK].isListening) {
+		CBLogError("SECOND LISTEN FAIL");
+		exit(EXIT_FAILURE);
+	}*/
+	// Start connection
+	CBRunOnEventLoop(commConnect->eventLoop, CBNetworkCommunicatorTryConnectionsVoid, commConnect, true);
+	// Wait until the network tests end for all CBNetworkCommunicators.
+	pthread_mutex_lock(&nodeEndMutex);
+	if (ended != 3)
+		pthread_cond_wait(&nodeEndCond, &nodeEndMutex);
+	pthread_mutex_unlock(&nodeEndMutex);
+	/*
+	// Check addresses in the first listening CBNetworkCommunicator
+	if (commListen->addresses->addrNum != 1) { // Only L2 should go back to addresses. CN is private
+		CBLogError("ADDRESS DISCOVERY LISTEN ONE ADDR NUM FAIL %i != 1", commListen->addresses->addrNum);
+		exit(EXIT_FAILURE);
+	}
+	if(! CBNetworkAddressManagerGotNetworkAddress(commListen->addresses, addrListen2)){
+		CBLogError("ADDRESS DISCOVERY LISTEN ONE LISTEN TWO FAIL");
+		exit(EXIT_FAILURE);
+	}*//*
+	// Check the addresses in the second.
+	if (commListen->addresses->addrNum != 1) {
+		CBLogError("ADDRESS DISCOVERY LISTEN TWO ADDR NUM FAIL");
+		exit(EXIT_FAILURE);
+	}
+	if (! CBNetworkAddressManagerGotNetworkAddress(commListen2->addresses, addrListen)){
+		CBLogError("ADDRESS DISCOVERY LISTEN TWO LISTEN ONE FAIL");
+		exit(EXIT_FAILURE);
+	}*/
+	// And lastly the connecting CBNetworkCommunicator
+	if (commConnect->addresses->addrNum != 2) {
+		CBLogError("ADDRESS DISCOVERY CONNECT ADDR NUM FAIL");
+		exit(EXIT_FAILURE);
+	}
+	/*addrListen->bucketSet = false;
+	if (! CBNetworkAddressManagerGotNetworkAddress(commConnect->addresses, addrListen)){
+		CBLogError("ADDRESS DISCOVERY CONNECT LISTEN ONE FAIL");
+		exit(EXIT_FAILURE);
+	}*/
+	/*addrListen2->bucketSet = false;
+	if (! CBNetworkAddressManagerGotNetworkAddress(commConnect->addresses, addrListen2)){
+		CBLogError("ADDRESS DISCOVERY CONNECT LISTEN TWO FAIL");
+		exit(EXIT_FAILURE);
+	}*/
+	// Release all final objects.
+	//CBReleaseObject(addrListen);
+	//CBReleaseObject(addrListen2);
+	CBReleaseObject(addrConnect);
+	//CBReleaseObject(commListen);
+	//CBReleaseObject(commListen2);
+	CBReleaseObject(commConnect);
+	pthread_mutex_destroy(&tester.testingMutex);
+	return EXIT_SUCCESS;
+}
